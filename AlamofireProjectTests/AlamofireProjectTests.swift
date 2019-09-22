a01158d05c5aadafc21852fe8b83d6440cc39881
Josue Ardon <josueardon.developer@gmail.com>
2019-09-22 13:22:09 -0600

    Init

---
+++ AlamofireProjectTests/AlamofireProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  AlamofireProjectTests.swift
+//  AlamofireProjectTests
+//
+//  Created by Josue Ardon on 9/20/19.
+//  Copyright © 2019 Josue Ardon. All rights reserved.
+//
+
+import XCTest
+@testable import AlamofireProject
+
+class AlamofireProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,30 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+source 'https://github.com/CocoaPods/Specs.git' 
+platform :ios, '10.0'
+
+target 'AlamofireProject' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AlamofireProject
+
+  # alamofire
+  pod 'Alamofire', '~> 4.5'
+  #Siwfty son
+  pod 'SwiftyJSON', '~> 4.0'
+  #SDWImage
+  pod 'SDWebImage', '~> 5.0'
+
+  target 'AlamofireProjectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AlamofireProjectUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
